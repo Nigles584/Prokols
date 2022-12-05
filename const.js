7506292f9e771cca6119e5a4e6ddb531f1cb729e
n1gles <zakhar.morgenshtern_2004@mail.ru>
2022-12-05 19:40:16 +0300

    init

---
+++ const.js
@@ -0,0 +1,38 @@
+const commands = `
+/start - перезапустить бота
+/help - Команды бота
+/education - ссылки на курсы с языками программирования
+/download_materials - файлы для быстрой загрузки
+`
+const text1 = `
+Курс по Python для новичков <a href="https://stepik.org/course/58852/info">ссылка</a>
+А так же полезные видеоматериалы <a href="https://www.youtube.com/playlist?list=PLQAt0m1f9OHvv2wxPGSCWjgy1qER_FvB6">ссылка</a>
+Ну и про литературу я тоже не забыл <a href="https://wombat.org.ua/AByteOfPython/AByteofPythonRussian-2.02.pdf">ссылка</a>
+`
+const text2 = `
+Курс по C# для новичков <a href="https://ulearn.me/">ссылка</a>
+А так же полезные видеоматериалы <a href="https://www.youtube.com/playlist?list=PLDyJYA6aTY1laYPs6iS-SrYl9DZLVCUKr">ссылка</a>
+Ну и про литературу я тоже не забыл <a href="https://vk.com/doc44301783_541532941?hash=QxXTtl34WQmpcsY73A8rDAuPH8d1I6NU0duuzaslSns&dl=yEWKcnazhYmD3pxT9vLZzO52lTbTX3gu3e9mtvneUg4">ссылка</a>
+`
+const text3 = `
+Курс по JS для новичков <a href="https://learn.javascript.ru/">ссылка</a>
+А так же полезные видеоматериалы <a href="https://www.youtube.com/playlist?list=PLM6XATa8CAG7DDIBjNVd78Fv5Ueo930IV">ссылка</a>
+Ну и про литературу я тоже не забыл <a href="https://vk.com/doc44301783_428433061?hash=4K2PkwMSsjtrNaIfzEWruVHDf4Qxa00fnwuVzqg2uaP&dl=Z4r9gHrCYoSvd5QeXBkqPBEFRETUcF3jrBAu8FzywDP">ссылка</a>
+`
+const text4 = `
+Cсылка для скачивания книги A byte of Python <a href="https://drive.google.com/uc?export=download&id=1TGYDXlVMPkXzZhp5W2QIj6OEg1ligvub">ссылка</a>
+`
+const text5 = `
+Cсылка для скачивания книги Программирование для начинающих на C# <a href="https://drive.google.com/uc?export=download&id=1j8afOKdFM0gWwA5zNYH3bsRi_HtTWNHT">ссылка</a>
+`
+const text6 = `
+Cсылка для скачивания книги Программирование для начинающих на C# <a href="https://drive.google.com/uc?export=download&id=11kGbmT0bHm_i37mVEYCIeVcqvqfhnTsu">ссылка</a>
+`
+
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
+module.exports.text4 = text4
+module.exports.text5 = text5
+module.exports.text6 = text6
---
+++ index.js
@@ -0,0 +1,67 @@
+const { Telegraf, Markup } = require('telegraf')
+require('dotenv').config()
+const text = require('./const')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.on((ctx) => console.log(ctx.message))
+bot.start((ctx) => ctx.replyWithSticker('CAACAgIAAxkBAAEGrZtjjJQ4tULqEm8Oq4gzz6IR-HHJ1gACcRIAAuKKkEpQfmU3XqesKSsE') + ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!` + ' Я могу поделиться с тобой информацией, которая поможет тебе изучить парочку интересных языков программирования'));
+bot.help((ctx) => ctx.reply(text.commands))
+
+bot.command('education', async (ctx) => {
+	try {
+		await ctx.replyWithHTML('<b>Курсы, которые тебе помогут начать свой путь в программировании</b>', Markup.inlineKeyboard(
+			[
+				[Markup.button.callback('Python', 'btn_1'), Markup.button.callback('C#', 'btn_2'), Markup.button.callback('JavaScript', 'btn_3')]
+			]
+		))
+	} catch (e) {
+		console.error(e)
+	}
+})
+
+bot.command('download_materials', async (ctx) => {
+	try {
+		await ctx.replyWithHTML('<i>Файлы с книгамии по программированию</i>', Markup.inlineKeyboard(
+			[
+				[Markup.button.callback('Python book', 'btn_4'), Markup.button.callback('C# book', 'btn_5'), Markup.button.callback('JavaScript book', 'btn_6')]
+			]
+		))
+	} catch (e) {
+		console.error(e)
+	}
+})
+
+function addActionBot(name, src, text) { 
+	bot.action(name, async (ctx) => {
+		try {
+			await ctx.answerCbQuery()
+			if (src !== false) {
+				await ctx.replyWithPhoto({
+					source: src
+				})
+			}
+			await ctx.replyWithHTML(text, {
+				disable_web_page_preview: true
+			})	
+		} catch (e) {
+			console.error(e)
+		}
+	})
+}
+
+
+
+
+addActionBot('btn_1', './img/1.jpg', text.text1)
+addActionBot('btn_2', './img/2.jpg', text.text2)
+addActionBot('btn_3', './img/3.jpg', text.text3)
+
+addActionBot('btn_4', './img/4.jpg', text.text4)
+addActionBot('btn_5', './img/5.jpg', text.text5)
+addActionBot('btn_6', './img/6.jpg', text.text6)
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
